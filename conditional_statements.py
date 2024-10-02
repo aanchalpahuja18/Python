45ed197a2afca959b62897fdad77c41c94af8c49
aanchalpahuja18 <aanchalpahuja34@gmail.com>
2024-10-02 12:56:40 +0530

    Conditional Statements in Python

---
+++ conditional_statements.py
@@ -0,0 +1,42 @@
+#Example 1: if-elif-else:
+
+
+# age = int(input("Please enter your age: "))
+
+# if age >= 18:
+#     print("You are now signed up!")
+# elif age < 0:
+#     print("You haven't born yet!")
+# elif age >= 100:
+#     print("You are too old to signup!")
+# else:
+#     print("You must be 18+ to signup!")
+
+
+#Example 2: if-else
+
+
+# response = input("Would you like to have some food? (Y/N)")
+
+# if response == 'Y':
+#     print("Have some food!")
+# else:
+#     print("No food for you!")
+
+#Example 3: if-else
+
+# name = input("Enter your name!")
+
+# if name == "":
+#     print("You haven't entered anything! Do it rn")
+# else:
+#     print(f"Helloo {name}")
+
+#Example 4: Boolean variables inside if stmnt
+
+for_sale = False
+
+if for_sale:
+    print("This item is for sale!")
+else:
+    print("This item is NOT for sale!")
