f68d4f40fca1dfc87e80b842f4cdec65c8f040a1
aanchalpahuja18 <aanchalpahuja34@gmail.com>
2024-10-19 14:43:51 +0530

    Modules in Python

---
+++ module.py
@@ -0,0 +1,20 @@
+# Modules in Python
+
+import math
+# import math as m
+
+# To include a specific function / variable we have below syntax: 
+# from math import pi
+
+# print(math.pi)
+
+
+import example_module as eg
+
+result = eg.pi
+result = eg.square(3)
+result = eg.cube(5)
+result = eg.circumference(10)
+result = eg.area(10)
+
+print(result)
